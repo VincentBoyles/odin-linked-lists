40f3ea267a4faf9482c90ca6762082d3f227a15e
VincentBoyles <boylesvincentmanuel@gmail.com>
2025-12-10 18:38:23 +0800

    main file, linklist and node file

---
+++ lib/LinkedList.rb
@@ -0,0 +1,37 @@
+class LinkedList
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at
+  end
+
+  def pop
+  end
+
+  def contrains?(value)
+  end
+
+  def find
+  end
+
+  def to_s
+  end
+
+  def insert_at(value, index)
+  end
+
+  def remove_at(index)
+  end
+end
